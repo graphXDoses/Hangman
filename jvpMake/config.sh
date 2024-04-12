2491fc29f26d90494506c1a0bef64150447b41a1
graphXDoses <xridoses@gmail.com>
2024-04-12 17:41:01 +0300

    Initial commit.

---
+++ jvpMake/config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Define directory paths and variables to be exported.
+export build_dir="./build/"
+export root_file="AppEntry"
+export project_name="Hangman"
+export project_dir="./Projects/$project_name/src/"
+export project_script="./jvpMake/build.sh"
